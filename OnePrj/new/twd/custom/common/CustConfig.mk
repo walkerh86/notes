94abb5b253f93fa9e8207ce2e12ead9872d5337e
hcj <hcj@twd>
2014-09-26 18:57:07 +0800

    update patch

--- OnePrj/new/twd/custom/common/CustConfig.mk
+++
@@ -1,7 +0,0 @@
-#common for all custom begin
-SHUT_ANIM = twd/custom/$(TWD_PROJECT)/$(TWD_CUST_NAME)/prebuilt/pwranim/shutanimation.zip
-ifeq ($(SHUT_ANIM), $(wildcard $(SHUT_ANIM)))
-TWD_PRODUCT_PROPERTY_OVERRIDES += \
-    ro.operator.optr=CUST
-endif
-#common for all custom end 
--- OnePrj/new/twd/custom/ty702_jty_hspa/XIAXIN_N98/CustConfig.mk
+++
@@ -1,8 +0,0 @@
-#+ by hcj for customize
-
-#var for additional package
-TWD_PRODUCT_PACKAGES := 
-
-#var for override property or addon
-TWD_PRODUCT_PROPERTY_OVERRIDES += \
-	persist.sys.timezone=Asia/Shanghai
